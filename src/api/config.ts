edc4be8e4b6bd1b29f55257b8322020cc726a577
Шумкар Зикиров <tezjarnama.ru@gmail.com>
2025-04-02 11:02:36 +0330

    api

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,8 +1,8 @@
 const API_KEY = 'fb942484b79cba228471672d6c79a178'
 
 export const API_CONFIG = {
-	BASE_URL: 'http://api.openweathermap.org/data/2.5',
-	GEO: 'http://api.openweathermap.org/geo/1.0',
+	BASE_URL: 'https://api.openweathermap.org/data/2.5',
+	GEO: 'https://api.openweathermap.org/geo/1.0',
 	API_KEY: API_KEY,
 	DEFAULT_PARAMS: {
 		units: 'metric',
